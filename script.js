9ed63e95e0f5b0eb0d28d4a76da611e9a2c7b9a5
Akmal <akmalabdullayevmadaniyat@gmail.com>
2020-12-16 15:16:52 +0500

    JS_Start

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+var ismingiz = prompt('Ismingiz ')
+var familiyangiz = prompt('Familiyangiz')
+var misol1 = prompt('Misolni yeching:  '+'25 + 12')
+var misol2 = prompt('Misolni yeching: ' + '36 / 6')
+var misol3 = prompt('Misolni yeching: ' + '7 * 3')
+var misol4 = prompt('Misolni yeching: ' + '14 - 5')
+var misol5 = prompt('Misolni yeching: ' + '7 % 6')
+
+var ism = ('Sizning ismingiz: '+ ismingiz + ',' );
+var familiya = ('Sizning familiyangiz: ' + familiyangiz + ',');
+var misol1 = ('sizning javob: ' + misol1 + '  ' + 'To`gri javob ' + (25+12) + ',');
+var misol2 = ('sizning javob: ' + misol2 + '  ' + 'Tog`ri javob ' + (36 / 6) + ',');
+var misol3 = ('sizning javob: ' + misol3 + '  ' + 'Tog`ri javob ' + (7 * 3) + ',');
+var misol4 = ('sizning javob: ' + misol4 + '  ' + 'Tog`ri javob ' + (14 - 5) + ',');
+var misol5 = ('sizning javob: ' + misol5 + '  ' + 'Tog`ri javob ' + (7 % 6) + ',');
+
+
+console.log(ism, familiya, misol1, misol2, misol3, misol4, misol5);
